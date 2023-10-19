0e2992eed8799f095eaa8cfc6e10f8f0f1267fbd
Éverton Carvalho <evertonsnake@gmail.com>
2023-10-19 14:41:35 -0300

    Criação do Header da pagina HomeNoAuth - Header não autenticado

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,22 @@
 import Head from "next/head";
 import styles from "../styles/HomeNoAuth.module.scss";
+import HeaderNoAuth from "@/src/components/homeNoAuth/headerNoAuth";
 
 const HomeNoAuth = () => {
   return (
     <>
       <Head>
-        <title>Home</title>
+        <title>OneBtiFlix</title>
+        <link rel="shortcut icon" href="/favicon.svg" type="image/x-icon" />
+        <meta property="og:title" content="OneBitFlix" key="title" />
+        <meta
+          name="descritpion"
+          content="Tenha acesso aso melhores conteúdos de promgração de uma forma simples e fácil"
+        />
       </Head>
-      <main></main>
+      <main>
+        <HeaderNoAuth />
+      </main>
     </>
   );
 };
