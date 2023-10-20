1f8f0a2ccac9a34f45eff8a757aa9e07f4c95bbb
Éverton Carvalho <evertonsnake@gmail.com>
2023-10-20 13:17:27 -0300

    Header Generic

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,15 +6,19 @@ import CardsSection from "@/src/components/homeNoAuth/cardSection";
 import SlideSection from "@/src/components/homeNoAuth/slideSection";
 import { GetStaticProps } from "next";
 import courseService, { CourseType } from "@/src/services/courseService";
-import { ReactNode } from "react";
+import { ReactNode, useEffect } from "react";
 import Footer from "@/src/components/commom/footer";
-
+import AOS from "aos";
+import "aos/dist/aos.css";
 interface IndexPageProps {
   children?: ReactNode;
   course: CourseType[];
 }
 
 export default function HomeNoAuth({ course }: IndexPageProps) {
+  useEffect(() => {
+    AOS.init();
+  }, []);
   return (
     <>
       <Head>
@@ -27,12 +31,20 @@ export default function HomeNoAuth({ course }: IndexPageProps) {
         />
       </Head>
       <main>
-        <div className={styles.sectionBackground}>
+        <div
+          className={styles.sectionBackground}
+          data-aos="fade-zoom-in"
+          data-aos-duration="1600"
+        >
           <HeaderNoAuth />
           <PresentationSection />
         </div>
-        {/* <CardsSection /> */}
-        <SlideSection newestCourses={course} />
+        <div data-aos="fade-right" data-aos-duration="1200">
+          <CardsSection />
+        </div>
+        <div data-aos="fade-up" data-aos-duration="1350">
+          <SlideSection newestCourses={course} />
+        </div>
         <Footer />
       </main>
     </>
