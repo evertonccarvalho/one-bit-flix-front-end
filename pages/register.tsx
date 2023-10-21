53ac51e684361ce2bf218f9432861d79e664be40
Éverton Carvalho <evertonsnake@gmail.com>
2023-10-20 22:30:06 -0300

    Criação e configuração paginas registro e login

--- pages/register.tsx
+++ pages/register.tsx
@@ -1,19 +1,188 @@
 import HeaderGeneric from "@/src/components/commom/headerGeneric";
 import styles from "../styles/registerLogin.module.scss";
 import Head from "next/head";
+import { Container, Button, Form, FormGroup, Label, Input } from "reactstrap";
+import Script from "next/script";
+import Footer from "@/src/components/commom/footer";
+import { FormEvent, useState } from "react";
+import authService from "@/src/services/authService";
+import { useRouter } from "next/router";
+import ToastComponent from "@/src/components/commom/toast";
 
 export default function Register() {
+  const router = useRouter();
+  const [toastIsOpen, setToastIsOpen] = useState(false);
+  const [toastMessage, setToastMessage] = useState("");
+
+  const handleRegister = async (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    const formData = new FormData(event.currentTarget);
+    const firstName = formData.get("firstName")!.toString();
+    const lastName = formData.get("lastName")!.toString();
+    const phone = formData.get("phone")!.toString();
+    const birth = formData.get("birth")!.toString();
+    const email = formData.get("email")!.toString();
+    const password = formData.get("password")!.toString();
+    const confirmPassword = formData.get("confirmPassword")!.toString();
+    const params = { firstName, lastName, phone, birth, email, password };
+
+    if (password != confirmPassword) {
+      setToastIsOpen(true);
+      setTimeout(() => {
+        setToastIsOpen(false);
+      }, 1000 * 3);
+      setToastMessage("A senha e confirmação são diferentes!");
+      return;
+    }
+
+    const { data, status } = await authService.register(params);
+
+    if (status === 201) {
+      router.push("/login?registred=true");
+    } else {
+      setToastIsOpen(true);
+      setTimeout(() => {
+        setToastIsOpen(false);
+      }, 1000 * 3);
+      setToastMessage(data.message);
+    }
+  };
+
   return (
     <>
+      <Script src="https://jsuites.net/v4/jsuites.js" />
       <Head>
         <title>OneBitFlix - Registro</title>
+
         <link rel="shortcut icon" href="/favicon.svg" type="image/x-icon" />
       </Head>
-      <main>
+      <main className={styles.main}>
         <HeaderGeneric
           logoUrl="/"
           btnUrl="/login"
           btnContent="Quero fazer login"
+        />
+        <Container className="py-5 ">
+          <p className="text-center">
+            <strong>Bem-Vindo(a) ao OneBitFlix</strong>
+          </p>
+          <Form className={styles.form} onSubmit={handleRegister}>
+            <p className="text-center">
+              <strong>Faça a sua conta!</strong>
+            </p>
+            <FormGroup>
+              <Label for="firstName" className={styles.label}>
+                Nome:
+              </Label>
+              <Input
+                if="firstName"
+                name="firstName"
+                type="text"
+                placeholder="Qual o seu nome?"
+                required
+                maxLength={20}
+                className={styles.inputName}
+              />
+            </FormGroup>
+            <FormGroup>
+              <Label for="lastName" className={styles.label}>
+                Sobrenome:
+              </Label>
+              <Input
+                if="lastName"
+                name="lastName"
+                type="text"
+                placeholder="Qual o seu sobrenome?"
+                required
+                maxLength={20}
+                className={styles.inputName}
+              />
+            </FormGroup>
+            <FormGroup>
+              <Label for="phone" className={styles.label}>
+                Whatsapp / Telegram:
+              </Label>
+              <Input
+                if="phone"
+                name="phone"
+                type="tel"
+                placeholder="(xx) 9xxxx-xxxx"
+                data-mask="[-](00) 0 0000-0000"
+                required
+                maxLength={20}
+                className={styles.input}
+              />
+            </FormGroup>
+            <FormGroup>
+              <Label for="email" className={styles.label}>
+                E-mail
+              </Label>
+              <Input
+                if="email"
+                name="email"
+                type="email"
+                placeholder="Digite o seu e-mail"
+                required
+                maxLength={20}
+                className={styles.input}
+              />
+            </FormGroup>
+            <FormGroup>
+              <Label for="email" className={styles.label}>
+                Data de nascimento
+              </Label>
+              <Input
+                if="birth"
+                name="birth"
+                type="date"
+                min="1930-01-01"
+                max={Date.now()}
+                required
+                maxLength={10}
+                className={styles.input}
+              />
+            </FormGroup>
+            <FormGroup>
+              <Label for="password" className={styles.label}>
+                Senha
+              </Label>
+              <Input
+                if="password"
+                name="password"
+                type="password"
+                placeholder="Digita a sua senha"
+                required
+                minLength={6}
+                maxLength={20}
+                className={styles.input}
+              />
+            </FormGroup>{" "}
+            <FormGroup>
+              <Label for="confirmPassword" className={styles.label}>
+                Confirme sua senha
+              </Label>
+              <Input
+                if="confirmPassword"
+                name="confirmPassword"
+                type="password"
+                placeholder="Confirme a sua senha"
+                required
+                minLength={6}
+                maxLength={20}
+                className={styles.input}
+              />
+            </FormGroup>
+            <Button type="submit" outline className={styles.formBtn}>
+              Cadastrar
+            </Button>
+          </Form>
+        </Container>
+        <Footer />
+        <ToastComponent
+          color="bg-danger"
+          isOpen={toastIsOpen}
+          message={toastMessage}
         />
       </main>
     </>
