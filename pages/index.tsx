06f1fe843927da8d66ce3d869a61400bc606e4a7
Éverton Carvalho <evertonsnake@gmail.com>
2024-01-03 15:45:42 -0300

    deploy ok e previewpng

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,62 +1,73 @@
-import Head from "next/head";
-import styles from "../styles/HomeNoAuth.module.scss";
-import HeaderNoAuth from "@/src/components/homeNoAuth/headerNoAuth";
-import PresentationSection from "@/src/components/homeNoAuth/presentationSection";
-import CardsSection from "@/src/components/homeNoAuth/cardSection";
-import SlideSection from "@/src/components/homeNoAuth/slideSection";
-import { GetStaticProps } from "next";
-import courseService, { CourseType } from "@/src/services/courseService";
-import { ReactNode, useEffect } from "react";
-import Footer from "@/src/components/commom/footer";
-import AOS from "aos";
-import "aos/dist/aos.css";
+import Head from 'next/head';
+import styles from '../styles/HomeNoAuth.module.scss';
+import HeaderNoAuth from '@/src/components/homeNoAuth/headerNoAuth';
+import PresentationSection from '@/src/components/homeNoAuth/presentationSection';
+import CardsSection from '@/src/components/homeNoAuth/cardSection';
+import SlideSection from '@/src/components/homeNoAuth/slideSection';
+import { CourseType } from '@/src/services/courseService';
+import { ReactNode, useEffect } from 'react';
+import Footer from '@/src/components/commom/footer';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 interface IndexPageProps {
-  children?: ReactNode;
-  course: CourseType[];
+	children?: ReactNode;
+	course: CourseType[];
 }
 
 export default function HomeNoAuth({ course }: IndexPageProps) {
-  useEffect(() => {
-    AOS.init();
-  }, []);
-  return (
-    <>
-      <Head>
-        <title>OneBtiFlix</title>
-        <link rel="shortcut icon" href="/favicon.svg" type="image/x-icon" />
-        <meta property="og:title" content="OneBitFlix" key="title" />
-        <meta
-          name="descritpion"
-          content="Tenha acesso aso melhores conteúdos de promgração de uma forma simples e fácil"
-        />
-      </Head>
-      <main>
-        <div
-          className={styles.sectionBackground}
-          data-aos="fade-zoom-in"
-          data-aos-duration="1600"
-        >
-          <HeaderNoAuth />
-          <PresentationSection />
-        </div>
-        <div data-aos="fade-right" data-aos-duration="1200">
-          <CardsSection />
-        </div>
-        <div data-aos="fade-up" data-aos-duration="1350">
-          <SlideSection newestCourses={course} />
-        </div>
-        <Footer />
-      </main>
-    </>
-  );
+	useEffect(() => {
+		AOS.init();
+	}, []);
+	return (
+		<>
+			<Head>
+				<title>OneBtiFlix</title>
+				<link rel="shortcut icon" href="/favicon.svg" type="image/x-icon" />
+				<meta property="og:title" content="OneBitFlix" key="title" />
+				<meta
+					name="descritpion"
+					content="Tenha acesso aso melhores conteúdos de promgração de uma forma simples e fácil"
+				/>
+			</Head>
+			<main>
+				<div
+					className={styles.sectionBackground}
+					data-aos="fade-zoom-in"
+					data-aos-duration="1600"
+				>
+					<HeaderNoAuth />
+					<PresentationSection />
+				</div>
+				<div data-aos="fade-right" data-aos-duration="1200">
+					<CardsSection />
+				</div>
+				<div data-aos="fade-up" data-aos-duration="1350">
+					<SlideSection newestCourses={course} />
+				</div>
+				<Footer />
+			</main>
+		</>
+	);
 }
 
-export const getStaticProps: GetStaticProps = async () => {
-  const res = await courseService.getNewestCourses();
-  return {
-    props: {
-      course: res.data,
-    },
-    revalidate: 3600 * 24,
-  };
-};
+export async function getStaticProps() {
+	try {
+		const res = await fetch('URL_DA_SUA_API_AQUI');
+		const data = await res.json();
+
+		return {
+			props: {
+				course: data || [], // Garantindo que data seja um array ou um valor vazio
+			},
+			revalidate: 3600 * 24,
+		};
+	} catch (error) {
+		console.error('Error fetching data:', error);
+		return {
+			props: {
+				course: [], // Definindo um array vazio em caso de erro na requisição
+			},
+			revalidate: 3600 * 24,
+		};
+	}
+}
--- src/components/commom/slideComponent/index.tsx
+++ src/components/commom/slideComponent/index.tsx
@@ -1,59 +1,67 @@
-import { Splide, SplideSlide } from "../../../../node_modules/@splidejs/react-splide";
-import "@splidejs/splide/dist/css/splide.min.css";
-import { CourseType } from "@/src/services/courseService";
-import SlideCard from "../slideCard";
+import {
+	Splide,
+	SplideSlide,
+} from '../../../../node_modules/@splidejs/react-splide';
+import '@splidejs/splide/dist/css/splide.min.css';
+import { CourseType } from '@/src/services/courseService';
+import SlideCard from '../slideCard';
 
 interface props {
-  course: CourseType[];
+	course: CourseType[];
 }
 
 export default function SlideComponent({ course }: props) {
-  let slideCount = 0;
-  if (course.length > 4) {
-    slideCount = 4;
-  } else if (course) {
-    slideCount = course.length;
-  }
+	let slideCount = 0;
+	let courseArray = Array.isArray(course) ? course : [];
 
-  return (
-    <>
-      <div>
-        <Splide
-          className="d-flex flex-column justify-content-between  py-4"
-          options={{
-            type: "loop",
-            perPage: slideCount,
-            perMove: 1,
-            width: slideCount * 300,
-            pagination: true,
-            arrows: course.length > 4 ? true : false,
-            drag: course.length > 4 ? true : false,
-            breakpoints: {
-              1200: {
-                perPage: slideCount >= 2 ? 2 : 1,
-                width: slideCount >= 2 ? 600 : 300,
-                arrows: course.length > 2 ? true : false,
-                drag: course.length > 2 ? true : false,
-              },
-              600: {
-                perPage: 1,
-                width: 300,
-                arrows: course.length > 1 ? true : false,
-                drag: course.length > 1 ? true : false,
-              },
-              300: {
-                with: 260,
-              },
-            },
-          }}
-        >
-          {course?.map((course) => (
-            <SplideSlide key={course.id}>
-              <SlideCard course={course} />
-            </SplideSlide>
-          ))}
-        </Splide>
-      </div>
-    </>
-  );
+	if (courseArray.length > 4) {
+		slideCount = 4;
+	} else {
+		slideCount = courseArray.length;
+	}
+	return (
+		<>
+			<div>
+				<Splide
+					className="d-flex flex-column justify-content-between  py-4"
+					options={{
+						type: 'loop',
+						perPage: slideCount,
+						perMove: 1,
+						width: slideCount * 300,
+						pagination: true,
+						arrows: course.length > 4 ? true : false,
+						drag: course.length > 4 ? true : false,
+						breakpoints: {
+							1200: {
+								perPage: slideCount >= 2 ? 2 : 1,
+								width: slideCount >= 2 ? 600 : 300,
+								arrows: course.length > 2 ? true : false,
+								drag: course.length > 2 ? true : false,
+							},
+							600: {
+								perPage: 1,
+								width: 300,
+								arrows: course.length > 1 ? true : false,
+								drag: course.length > 1 ? true : false,
+							},
+							300: {
+								with: 260,
+							},
+						},
+					}}
+				>
+					{courseArray && courseArray.length > 0 ? (
+						courseArray.map((course) => (
+							<SplideSlide key={course.id}>
+								<SlideCard course={course} />
+							</SplideSlide>
+						))
+					) : (
+						<p>No data available</p>
+					)}
+				</Splide>
+			</div>
+		</>
+	);
 }
